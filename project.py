169bcc45ab518932ab106a3f2f41abfe7b33c9ea
utkarsh pandey <152366191+putkarsh81@users.noreply.github.com>
2025-04-11 11:18:35 +0530

    Update project.py

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load the data
+# Load the datas
 file_path = 'putkarsh624_17436592088213003.csv'
 df = pd.read_csv(file_path)
 
